6b11a0a53a139c9d6b336c5efa6703cf1ec76b4d
LenaShadrina <lena.shadrina.1987@mail.ru>
2024-06-09 12:34:52 +0300

    correct mistake

--- homework_4.py
+++ homework_4.py
@@ -1,11 +1,12 @@
 if __name__ == "__main__":
     def auth(func):
-        def check_login(rub, login):
+        def check_login(*args, **kwargs):
             log1 = "N123"
             if log1 != login:
                 print("Доступ запрещен.")
             else:
-                return func(rub, login)
+                value = func(*args, **kwargs)
+                return value
         return check_login
     @auth
     def contribution(rub, login):
